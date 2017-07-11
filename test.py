38f75d6e09655fb21daedc390d0d2f7c0df067c9
yuzi sekiguchi <tistnmn@gmail.com>
2017-07-11 11:37:22 +0900

    とりあえずのコミットテスト。昨日作成したスクリプト。文字化けしないかどうか

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("hello, world!")
+import requests
+
+#VMware NSX のパラメータを宣言
+url = 'https://192.168.0.20/api/2.0/vdn/controller'
+user = 'admin'
+pwd =  'M1nd!iaas'
+
+res = requests.get(url, auth=(user,pwd), verify=False)
+print(res)
