fe7901b659309a75430d723742528a4072df1ed5
yuzi sekiguchi <tistnmn@gmail.com>
2017-07-14 16:44:08 +0900

    パラメータをいったん外す。 HTTP 415 error 回避

--- deploy/test.py
+++ deploy/test.py
@@ -1,12 +1,12 @@
 import requests
 import json
 
-url = "https://192.168.0.20/api/4.0/edges"
+url = "https://192.168.0.20/api/4.0/edges/"
 user = "admin"
 pwd =  "M1nd!iaas"
 headers = {"Accept": "application/json"}
 
 
-res = requests.post(url, auth=(user,pwd), headers=headers, verify=False)
+res = requests.post(url, auth=(user,pwd), verify=False)
 
 print(res)
