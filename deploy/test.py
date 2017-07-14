9be869f7fccfd98fa61c92d507990e66d09039c6
yuzi sekiguchi <tistnmn@gmail.com>
2017-07-14 14:38:32 +0900

    ヘッダを追加。JSON　情報がほしい

--- deploy/test.py
+++ deploy/test.py
@@ -3,6 +3,8 @@ import requests
 url = 'https://192.168.0.20/api/2.0/vdn/controller'
 user = 'admin'
 pwd =  'M1nd!iaas'
+headers = {"Accept": "application/json"}
 
-res = requests.post(url, auth=(user,pwd), verify=False)
+
+res = requests.get(url, auth=(user,pwd), headers, verify=False)
 print(res)
