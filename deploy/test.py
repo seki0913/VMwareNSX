86ba1a0eca6d34be906e472f552c18e0d838162a
yuzi sekiguchi <tistnmn@gmail.com>
2017-07-14 11:49:44 +0900

    Post Changed

--- deploy/test.py
+++ deploy/test.py
@@ -4,4 +4,5 @@ url = 'https://192.168.0.20/api/2.0/vdn/controller'
 user = 'admin'
 pwd =  'M1nd!iaas'
 
-res = requests.get(url, auth=(user,pwd), verify=False)
+res = requests.post(url, auth=(user,pwd), verify=False)
+print(res)
--- test.py
+++ test.py
@@ -4,5 +4,8 @@ url = 'https://192.168.0.20/api/2.0/vdn/controller'
 user = 'admin'
 pwd =  'M1nd!iaas'
 
-res = requests.get(url, auth=(user,pwd), verify=False)
-print(res)
+# GET Method
+# res = requests.get(url, auth=(user,pwd), verify=False)
+# print(res)
+
+# 
