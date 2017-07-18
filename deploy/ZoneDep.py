9d50b8510b3db4d5c0c231feaca42b11c95557a9
administrator@CURRY.LOCAL <administrator@CURRY.LOCAL>
2017-07-18 17:45:15 +0900

    XML complete

--- deploy/ZoneDep.py
+++ deploy/ZoneDep.py
@@ -1,12 +1,14 @@
-import request, json
+import requests, json
 
 
-url = "https://192.168.0.20/api/4.0/edges/"
+url = "https://192.168.0.20/api/2.0/vdn/scopes?inUniveral=true"
 user = "admin"
 pwd =  "M1nd!iaas"
-headers = {"Accept": "application/json"}
+headers = {"content-type": "application/xml"}
 method = "POST"
 
-obj = {
-    
-}
+obj = "<vdnScope><name>test-nano!</name><clusters><cluster><cluster><objectId>domain-c74</objectId></cluster></cluster></clusters></vdnScope>"
+
+req = requests.post(url, auth=(user,pwd), data=obj, headers=headers, verify=False)
+
+print(req)
