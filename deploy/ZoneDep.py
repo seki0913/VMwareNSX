aac7d71e9afbd47637d1709d284470307dae6bea
administrator@CURRY.LOCAL <administrator@CURRY.LOCAL>
2017-07-18 15:58:44 +0900

    Scope JSON

---
+++ deploy/ZoneDep.py
@@ -0,0 +1,12 @@
+import request, json
+
+
+url = "https://192.168.0.20/api/4.0/edges/"
+user = "admin"
+pwd =  "M1nd!iaas"
+headers = {"Accept": "application/json"}
+method = "POST"
+
+obj = {
+    
+}
--- deploy/test.py
+++ deploy/test.py
@@ -1,12 +1,14 @@
 import requests
 import json
 
-url = "https://192.168.0.20/api/4.0/edges/"
+url = "https://192.168.0.20/api/2.0/vdn/scopes"
 user = "admin"
 pwd =  "M1nd!iaas"
 headers = {"Accept": "application/json"}
 
 
-res = requests.post(url, auth=(user,pwd), verify=False)
+res = requests.get(url, auth=(user,pwd), headers=headers, verify=False).json()
+open_file = open("./output_scopes.json", "w")
+json.dump(res, open_file)
 
 print(res)
