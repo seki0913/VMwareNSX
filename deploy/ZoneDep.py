24ce9e7f8a6f9ef50660a684c5add7c5406bcf80
administrator@CURRY.LOCAL <administrator@CURRY.LOCAL>
2017-07-19 10:43:06 +0900

    JSON うまくいかない

--- deploy/ZoneDep.py
+++ deploy/ZoneDep.py
@@ -4,11 +4,24 @@ import requests, json
 url = "https://192.168.0.20/api/2.0/vdn/scopes?inUniveral=true"
 user = "admin"
 pwd =  "M1nd!iaas"
-headers = {"content-type": "application/xml"}
+headers = {"content-type": "application/json"}
 method = "POST"
 
-obj = "<vdnScope><name>test-nano!</name><clusters><cluster><cluster><objectId>domain-c74</objectId></cluster></cluster></clusters></vdnScope>"
+obj = {
+    "vdnScope": [
+        "name": "test-dazo!",
+        "clusters": [
+            "cluster": {
+                "cluster": {
+                    "objectId":"domain-c74"
+                }
+            }
+        ]
+    ]
+}
 
-req = requests.post(url, auth=(user,pwd), data=obj, headers=headers, verify=False)
+obj_json = json.dumps(obj)
+
+req = requests.post(url, auth=(user,pwd), data=obj_json, headers=headers, verify=False)
 
 print(req)
